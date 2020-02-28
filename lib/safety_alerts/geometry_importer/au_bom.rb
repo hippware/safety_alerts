7075212eb182692da2f772388d350d21430aae24
Bernard Duggan <bernard@hippware.com>
2020-02-29 09:16:54 +1100

    Add missing geometry importer

---
+++ lib/safety_alerts/geometry_importer/au_bom.rb
@@ -0,0 +1,45 @@
+require 'json'
+require 'net/ftp'
+require 'nokogiri'
+require 'pathname'
+require 'rgeo/geo_json'
+require 'rgeo/shapefile'
+require 'tempfile'
+
+module SafetyAlerts
+  module GeometryImporter
+      module AU_BOM
+          def self.run
+              count = 0
+              AlertGeometryDB.with_connection('AU_BOM') do |db|
+                  ftp = Net::FTP.new('ftp.bom.gov.au')
+                  ftp.login
+                  ftp.chdir('anon/home/adfd/spatial')
+                  files = ftp.nlst('*.shp')
+
+                  files.each do |f|
+                      base = Pathname.new(f).basename('.shp').to_s + '.'
+                      ['shp', 'shx', 'dbf'].each do |ext|
+                          puts "Getting #{base + ext}"
+                          ftp.getbinaryfile(base + ext, '/tmp/source.' + ext)
+                      end
+
+                      RGeo::Shapefile::Reader.open('/tmp/source.shp') do |file|
+                          file.each do |record|
+                              if record.attributes["AAC"] then
+                                  db.insert_geometry(
+                                      id: record.attributes["AAC"],
+                                      geometry: record.geometry,
+                                      data: '{}'
+                                  )
+                                  count += 1
+                              end
+                          end
+                      end
+                  end
+              end
+              count
+          end
+      end
+  end
+end
