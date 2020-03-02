000926ab9b76183066a26f30874fe297e928203e
Phil Toland <phil.toland@gmail.com>
2020-03-02 14:20:19 -0700

    Clean up the AU_BOM geometry importer

--- lib/safety_alerts/geometry_importer/au_bom.rb
+++ lib/safety_alerts/geometry_importer/au_bom.rb
@@ -7,39 +7,37 @@ require 'rgeo/shapefile'
 require 'tempfile'
 
 module SafetyAlerts
-  module GeometryImporter
-      module AU_BOM
-          def self.run
-              count = 0
-              AlertGeometryDB.with_connection('AU_BOM') do |db|
-                  ftp = Net::FTP.new('ftp.bom.gov.au')
-                  ftp.login
-                  ftp.chdir('anon/home/adfd/spatial')
-                  files = ftp.nlst('*.shp')
+  module GeometryImporter::AU_BOM
+    def self.run(db)
+      count = 0
 
-                  files.each do |f|
-                      base = Pathname.new(f).basename('.shp').to_s + '.'
-                      ['shp', 'shx', 'dbf'].each do |ext|
-                          puts "Getting #{base + ext}"
-                          ftp.getbinaryfile(base + ext, '/tmp/source.' + ext)
-                      end
+      ftp = Net::FTP.new('ftp.bom.gov.au')
+      ftp.login
+      ftp.chdir('anon/home/adfd/spatial')
+      files = ftp.nlst('*.shp')
 
-                      RGeo::Shapefile::Reader.open('/tmp/source.shp') do |file|
-                          file.each do |record|
-                              if record.attributes["AAC"] then
-                                  db.insert_geometry(
-                                      id: record.attributes["AAC"],
-                                      geometry: record.geometry,
-                                      data: '{}'
-                                  )
-                                  count += 1
-                              end
-                          end
-                      end
-                  end
-              end
-              count
+      files.each do |f|
+        base = Pathname.new(f).basename('.shp').to_s + '.'
+        ['shp', 'shx', 'dbf'].each do |ext|
+          puts "Getting #{base + ext}"
+          ftp.getbinaryfile(base + ext, '/tmp/source.' + ext)
+        end
+
+        RGeo::Shapefile::Reader.open('/tmp/source.shp') do |file|
+          file.each do |record|
+            if record.attributes["AAC"] then
+              db.insert_geometry(
+                id: record.attributes["AAC"],
+                geometry: record.geometry,
+                data: '{}'
+              )
+              count += 1
+            end
           end
+        end
       end
+
+      count
+    end
   end
 end
