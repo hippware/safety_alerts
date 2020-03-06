802ed2f76a4c27b6e60904bd1a6f729ffdbc7fe9
Phil Toland <phil.toland@gmail.com>
2020-03-06 15:56:48 -0700

    More CI fixes

--- lib/safety_alerts/db.rb
+++ lib/safety_alerts/db.rb
@@ -14,7 +14,8 @@ module SafetyAlerts
         host: ENV['WOCKY_DB_HOST'] || 'localhost',
         user: ENV['WOCKY_DB_USER'] || 'postgres',
         dbname: ENV['WOCKY_DB_NAME'] || 'wocky_dev',
-        password: Secrets.get_value('db-password') || ''
+        password: Secrets.get_value('db-password') ||
+                    ENV['WOCKY_DB_PASSWORD'] || ''
       )
     end
 
